f9117271202110a8f7a4a1c764c1205ac0829c01
Rizky Maulana <rizky1517@gmail.com>
2025-09-27 17:21:14 +0700

    feature(validation): add validation-service

---
+++ src/main/java/com/unindra/service/ValidationService.java
@@ -0,0 +1,25 @@
+package com.unindra.service;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ValidationService {
+    
+    private final Validator validator;
+
+    public void validate(Object request) {
+        Set<ConstraintViolation<Object>> constraintViolations = validator.validate(request);
+        if (constraintViolations.size() != 0) {
+            throw new ConstraintViolationException(constraintViolations);
+        }
+    }
+
+}
