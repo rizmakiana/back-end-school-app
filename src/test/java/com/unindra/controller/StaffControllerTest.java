a76993fb75df80437b9268eb981b047e8a3553f9
Rizky Maulana <rizky1517@gmail.com>
2025-09-28 16:00:00 +0700

    feature(test-staff): add test back end validation register staff

--- src/test/java/com/unindra/controller/StaffControllerTest.java
+++ src/test/java/com/unindra/controller/StaffControllerTest.java
@@ -75,7 +75,7 @@ public class StaffControllerTest {
 
                     assertEquals("Staff registration completed successfully.", response.getMessage());
                 });
-        
+
         Staff staff = repository.findByUsername(request.getUsername()).orElse(null);
         assertNotNull(staff);
         assertEquals(request.getEmail(), staff.getEmail());
@@ -107,7 +107,8 @@ public class StaffControllerTest {
                         .content(objectMapper.writeValueAsString(request))
 
         ).andExpectAll(
-                status().isOk()).andDo(result -> {
+                status().isOk()
+        ).andDo(result -> {
                     WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(),
                             new TypeReference<>() {
                             });
@@ -115,11 +116,200 @@ public class StaffControllerTest {
                     assertEquals("Registrasi staf berhasil diselesaikan.", response.getMessage());
                     assertNull(response.getErrors());
                 });
-        
+
         Staff staff = repository.findByUsername(request.getUsername()).orElse(null);
         assertNotNull(staff);
         assertEquals(request.getEmail(), staff.getEmail());
-        
+
     }
 
+    @Test
+    public void testRegisterFailPasswordnotEqualsEn() throws JsonProcessingException, Exception {
+        RegisterStaffRequest request = RegisterStaffRequest.builder()
+                .name("Zahra")
+                .gender(Gender.FEMALE)
+                .birthPlaceRegency("3201")
+                .birthDate("14")
+                .birthMonth(4)
+                .birthYear("2003")
+                .districtAddress("320101")
+                .address("Jatiasih No 17")
+                .username("zahra")
+                .password("rahasia")
+                .confirmPassword("rahasiabeda")
+                .email("zahra@gmail.com")
+                .phoneNumber("+6289278123")
+                .build();
+
+        mockMvc.perform(
+                post("/api/staff")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.ACCEPT_LANGUAGE, "en")
+                        .content(objectMapper.writeValueAsString(request))
+
+        ).andExpectAll(
+                status().isBadRequest()
+        ).andDo(result -> {
+                    WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(),
+                            new TypeReference<>() {
+                            });
+
+                            assertEquals("Password and confirm password do not match.", response.getErrors());
+                            assertNotNull(response.getErrors());
+                        });        
+    }
+
+    @Test
+    public void testRegisterFailInvalidDatesEn() throws JsonProcessingException, Exception {
+        RegisterStaffRequest request = RegisterStaffRequest.builder()
+                .name("Zahra")
+                .gender(Gender.FEMALE)
+                .birthPlaceRegency("3201")
+                .birthDate("30")
+                .birthMonth(2)
+                .birthYear("2003")
+                .districtAddress("320101")
+                .address("Jatiasih No 17")
+                .username("zahra")
+                .password("rahasia")
+                .confirmPassword("rahasia")
+                .email("zahra@gmail.com")
+                .phoneNumber("+6289278123")
+                .build();
+
+        mockMvc.perform(
+                post("/api/staff")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.ACCEPT_LANGUAGE, "en")
+                        .content(objectMapper.writeValueAsString(request))
+
+        ).andExpectAll(
+                status().isBadRequest()
+        ).andDo(result -> {
+                    WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(),
+                            new TypeReference<>() {
+                            });
+
+                            assertEquals("The birth date is invalid. Please check the date, month, and year.", response.getErrors());
+                            assertNotNull(response.getErrors());
+                        });        
+    }
+
+    @Test
+    public void testRegisterFailRegencyNotFoundEn() throws JsonProcessingException, Exception {
+        RegisterStaffRequest request = RegisterStaffRequest.builder()
+                .name("Zahra")
+                .gender(Gender.FEMALE)
+                .birthPlaceRegency("3200")
+                .birthDate("14")
+                .birthMonth(2)
+                .birthYear("2003")
+                .districtAddress("320101")
+                .address("Jatiasih No 17")
+                .username("zahra")
+                .password("rahasia")
+                .confirmPassword("rahasia")
+                .email("zahra@gmail.com")
+                .phoneNumber("+6289278123")
+                .build();
+
+        mockMvc.perform(
+                post("/api/staff")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.ACCEPT_LANGUAGE, "en")
+                        .content(objectMapper.writeValueAsString(request))
+
+        ).andExpectAll(
+                status().isNotFound()
+        ).andDo(result -> {
+                    WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(),
+                            new TypeReference<>() {
+                            });
+
+                            assertNotNull(response.getErrors());
+                            assertEquals("The specified birthplace regency could not be found.", response.getErrors());
+                        });        
+    }
+
+    @Test
+    public void testRegisterFailDistrictNotFoundEn() throws JsonProcessingException, Exception {
+        RegisterStaffRequest request = RegisterStaffRequest.builder()
+                .name("Zahra")
+                .gender(Gender.FEMALE)
+                .birthPlaceRegency("3201")
+                .birthDate("14")
+                .birthMonth(2)
+                .birthYear("2003")
+                .districtAddress("320100")
+                .address("Jatiasih No 17")
+                .username("zahra")
+                .password("rahasia")
+                .confirmPassword("rahasia")
+                .email("zahra@gmail.com")
+                .phoneNumber("+6289278123")
+                .build();
+
+        mockMvc.perform(
+                post("/api/staff")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.ACCEPT_LANGUAGE, "en")
+                        .content(objectMapper.writeValueAsString(request))
+
+        ).andExpectAll(
+                status().isNotFound()
+        ).andDo(result -> {
+                    WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(),
+                            new TypeReference<>() {
+                            });
+
+                            assertNotNull(response.getErrors());
+                            assertEquals("The specified district address could not be found.", response.getErrors());
+                        });        
+    }
+
+    @Test
+    public void testRegisterFailUsernameAlreadyExists() throws JsonProcessingException, Exception {
+        Staff staff = new Staff();
+        staff.setUsername("zahra");
+
+        repository.save(staff);
+
+        RegisterStaffRequest request = RegisterStaffRequest.builder()
+                .name("Zahra")
+                .gender(Gender.FEMALE)
+                .birthPlaceRegency("3201")
+                .birthDate("14")
+                .birthMonth(2)
+                .birthYear("2003")
+                .districtAddress("320101")
+                .address("Jatiasih No 17")
+                .username("zahra")
+                .password("rahasia")
+                .confirmPassword("rahasia")
+                .email("zahra@gmail.com")
+                .phoneNumber("+6289278123")
+                .build();
+
+        mockMvc.perform(
+                post("/api/staff")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.ACCEPT_LANGUAGE, "en")
+                        .content(objectMapper.writeValueAsString(request))
+
+        ).andExpectAll(
+                status().isBadRequest()
+        ).andDo(result -> {
+                    WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(),
+                            new TypeReference<>() {
+                            });
+
+                            assertNotNull(response.getErrors());
+                            assertEquals("The username is already registered.", response.getErrors());
+                        });        
+    }
 }
