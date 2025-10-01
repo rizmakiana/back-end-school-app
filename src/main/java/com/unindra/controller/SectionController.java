0cd4479fcd0c2ed014c74f3487c284e3892af4c9
Rizky Maulana <rizky1517@gmail.com>
2025-10-01 17:44:29 +0700

    feature(section): add create-section API

--- src/main/java/com/unindra/controller/SectionController.java
+++ src/main/java/com/unindra/controller/SectionController.java
@@ -3,12 +3,16 @@ package com.unindra.controller;
 import java.util.List;
 import java.util.Locale;
 
+import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.unindra.model.request.SectionRequest;
 import com.unindra.model.response.SectionResponse;
 import com.unindra.model.response.WebResponse;
 import com.unindra.service.SectionService;
@@ -22,11 +26,28 @@ public class SectionController {
 
     private final SectionService service;
 
+    private final MessageSource source;
+
     @GetMapping
     public ResponseEntity<WebResponse<List<SectionResponse>>> getAll(Authentication authentication, Locale locale) {
         return ResponseEntity.ok(
                 WebResponse.<List<SectionResponse>>builder().data(service.getAll())
                         .build());
+    }
+
+    @PostMapping
+    public ResponseEntity<WebResponse<String>> add(
+        Authentication authentication,
+        @RequestBody SectionRequest request,
+        Locale locale
+    ) {
+        
+        service.add(request, locale);
+        return ResponseEntity.ok(
+            WebResponse.<String>builder()
+                .message(source.getMessage("section.created", null, locale))
+                .build()
+        );
     }
 
 }
--- src/main/java/com/unindra/service/impl/SectionServiceImpl.java
+++ src/main/java/com/unindra/service/impl/SectionServiceImpl.java
@@ -4,11 +4,19 @@ import java.util.List;
 import java.util.Locale;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.unindra.entity.Classroom;
+import com.unindra.entity.Department;
+import com.unindra.entity.Section;
 import com.unindra.model.request.SectionRequest;
 import com.unindra.model.response.SectionResponse;
 import com.unindra.repository.SectionRepository;
+import com.unindra.service.ClassroomService;
+import com.unindra.service.DepartmentService;
 import com.unindra.service.SectionService;
+import com.unindra.service.ValidationService;
+import com.unindra.util.ExceptionUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +25,12 @@ import lombok.RequiredArgsConstructor;
 public class SectionServiceImpl implements SectionService {
 
     private final SectionRepository repository;
+
+    private final DepartmentService departmentService;
+
+    private final ClassroomService classroomService;
+
+    private final ValidationService validationService;
 
     @Override
     public List<SectionResponse> getAll() {
@@ -34,8 +48,22 @@ public class SectionServiceImpl implements SectionService {
 
     @Override
     public void add(SectionRequest request, Locale locale) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+        validationService.validate(request);
+
+        Department department = departmentService.findByName(request.getDepartmentName())
+                .orElseThrow(() -> ExceptionUtil.badRequest("department.notfound", locale));
+
+        Classroom classroom = classroomService.findByDepartmentAndName(department, request.getClassroomName())
+                .orElseThrow(() -> ExceptionUtil.badRequest("classroom.notfound", locale));
+
+        Character name = getSectionNameList(department, classroom.getName());
+        Section section = new Section();
+        section.setClassroom(classroom);
+        section.setName(name);
+        section.setCode(String.format("%s %s", classroom.getCode(), name));
+
+        repository.save(section);
+
     }
 
     @Override
@@ -48,6 +76,23 @@ public class SectionServiceImpl implements SectionService {
     public void delete(String id) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Transactional(readOnly = true)
+    public Character getSectionNameList(Department department, String classroomName) {
+        Classroom classroom = department.getClassrooms().stream()
+                .filter(c -> c.getName().equals(classroomName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Classroom not found"));
+
+        char last = 'A' - 1; // supaya kalau belum ada section, hasilnya 'A'
+        for (Section section : classroom.getSections()) {
+            if (section.getName() > last) {
+                last = section.getName();
+            }
+        }
+
+        return (char) (last + 1); // next huruf
     }
 
 }
