f9073476439ccab1b3cecbc8fe428d373e1411af
Rizky Maulana <rizky1517@gmail.com>
2025-10-05 23:00:51 +0700

    feature(teacher): add add-teacher API

--- src/main/java/com/unindra/controller/TeacherController.java
+++ src/main/java/com/unindra/controller/TeacherController.java
@@ -1,12 +1,18 @@
 package com.unindra.controller;
 
 import java.util.List;
+import java.util.Locale;
 
+import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.unindra.model.request.TeacherRequest;
 import com.unindra.model.response.TeacherResponse;
 import com.unindra.model.response.WebResponse;
 import com.unindra.service.TeacherService;
@@ -20,11 +26,25 @@ public class TeacherController {
 
     private final TeacherService service;
 
+    private final MessageSource source;
+
+    @PreAuthorize("hasRole('STAFF')")
     @GetMapping(path = "/staff/teachers")
     public void getAll() {
         ResponseEntity.ok(
                 WebResponse.<List<TeacherResponse>>builder()
                         .data(service.getAll())
                         .build());
+    }
+
+    @PreAuthorize("hasRole('STAFF')")
+    @PostMapping(path = "/staff/teachers")
+    public void add(@RequestBody TeacherRequest request, Locale locale) {
+        service.add(request, locale);
+        ResponseEntity.ok(
+            WebResponse.<String>builder()
+            .message(source.getMessage("teacher.created", null, locale))
+            .build()
+        );
     }
 }
