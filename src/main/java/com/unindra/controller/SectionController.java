22616fe788b13892457accb445e5b2bd949f3653
Rizky Maulana <rizky1517@gmail.com>
2025-10-05 09:28:57 +0700

    fix(controller): use spring-security flow at section

--- src/main/java/com/unindra/controller/SectionController.java
+++ src/main/java/com/unindra/controller/SectionController.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 
 import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -25,57 +25,58 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(path = "/api/sections")
+@RequestMapping(path = "/api")
 public class SectionController {
 
-        private final SectionService service;
+	private final SectionService service;
 
-        private final MessageSource source;
+	private final MessageSource source;
 
-        @GetMapping
-        public ResponseEntity<WebResponse<List<SectionResponse>>> getAll(Authentication authentication, Locale locale) {
-                return ResponseEntity.ok(
-                                WebResponse.<List<SectionResponse>>builder().data(service.getAll())
-                                                .build());
-        }
+	@PreAuthorize("hasRole('STAFF')")
+	@GetMapping(path = "/staff/sections")
+	public ResponseEntity<WebResponse<List<SectionResponse>>> getAll(Locale locale) {
+		return ResponseEntity.ok(
+				WebResponse.<List<SectionResponse>>builder().data(service.getAll())
+						.build());
+	}
 
-        @PostMapping
-        public ResponseEntity<WebResponse<String>> add(
-                        Authentication authentication,
-                        @RequestBody SectionRequest request,
-                        Locale locale) {
+	@PreAuthorize("hasRole('STAFF')")
+	@PostMapping(path = "/staff/sections")
+	public ResponseEntity<WebResponse<String>> add(
+			@RequestBody SectionRequest request,
+			Locale locale) {
 
-                service.add(request, locale);
-                return ResponseEntity.ok(
-                                WebResponse.<String>builder()
-                                                .message(source.getMessage("section.created", null, locale))
-                                                .build());
-        }
+		service.add(request, locale);
+		return ResponseEntity.ok(
+				WebResponse.<String>builder()
+						.message(source.getMessage("section.created", null, locale))
+						.build());
+	}
 
-        @DeleteMapping(path = "/{id}")
-        public ResponseEntity<WebResponse<String>> delete(
-                        Authentication authentication,
-                        @PathVariable String id,
-                        Locale locale) {
-                service.delete(id, locale);
-                return ResponseEntity.ok(
-                                WebResponse.<String>builder()
-                                                .message(source.getMessage("section.deleted", null, locale)).build());
-        }
+	@PreAuthorize("hasRole('STAFF')")
+	@DeleteMapping(path = "staff/sections/{id}")
+	public ResponseEntity<WebResponse<String>> delete(
+			@PathVariable String id,
+			Locale locale) {
+		service.delete(id, locale);
+		return ResponseEntity.ok(
+				WebResponse.<String>builder()
+						.message(source.getMessage("section.deleted", null, locale)).build());
+	}
 
-        @PatchMapping(path = "/{id}")
-        public ResponseEntity<WebResponse<String>> update(
-                        Authentication authentication,
-                        @PathVariable String id,
-                        @RequestBody SectionUpdateRequest request,
-                        Locale locale) {
+	@PreAuthorize("hasRole('STAFF')")
+	@PatchMapping(path = "/staff/sections/{id}")
+	public ResponseEntity<WebResponse<String>> update(
+			@PathVariable String id,
+			@RequestBody SectionUpdateRequest request,
+			Locale locale) {
 
-                service.update(id, request, locale);
-                return ResponseEntity.ok(
-                                WebResponse.<String>builder()
-                                                .message(source.getMessage("section.updated.succesfully", null, locale))
-                                                .build());
+		service.update(id, request, locale);
+		return ResponseEntity.ok(
+				WebResponse.<String>builder()
+						.message(source.getMessage("section.updated.succesfully", null, locale))
+						.build());
 
-        }
+	}
 
 }
