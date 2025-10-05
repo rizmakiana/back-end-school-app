1af42286444d4c005b9a64d18c6dabfbe28dc5c1
Rizky Maulana <rizky1517@gmail.com>
2025-10-05 23:03:23 +0700

    feature(teacher): add update-teacher API endpoint

--- src/main/java/com/unindra/controller/TeacherController.java
+++ src/main/java/com/unindra/controller/TeacherController.java
@@ -7,12 +7,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.unindra.model.request.TeacherRequest;
+import com.unindra.model.request.TeacherUpdate;
 import com.unindra.model.response.TeacherResponse;
 import com.unindra.model.response.WebResponse;
 import com.unindra.service.TeacherService;
@@ -44,6 +46,17 @@ public class TeacherController {
         ResponseEntity.ok(
             WebResponse.<String>builder()
             .message(source.getMessage("teacher.created", null, locale))
+            .build()
+        );
+    }
+
+    @PreAuthorize("hasRole('STAFF')")
+    @PostMapping(path = "/staff/teachers/{id}")
+    public void update(@PathVariable String id, TeacherUpdate request, Locale locale) {
+        service.update(id, request, locale);
+        ResponseEntity.ok(
+            WebResponse.<String>builder()
+            .message(source.getMessage("teacher.updated", null, locale))
             .build()
         );
     }
